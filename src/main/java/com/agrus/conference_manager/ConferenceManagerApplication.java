0579a2a52884bc23c67af19f238178f7a05e3fa0
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-12-22 22:15:40 +0200

    init

---
+++ src/main/java/com/agrus/conference_manager/ConferenceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.agrus.conference_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceManagerApplication.class, args);
+    }
+
+}
